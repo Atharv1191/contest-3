4b0fce5f82791a01f20b1ea8d8f511d3425cb1c8
Atharv <atharvj07@gmail.com>
2023-02-25 18:31:20 +0530

    main

--- script.js
+++ script.js
@@ -1,120 +1,50 @@
-const bookNameElement = document.getElementById('book-name');
-const issueToElement = document.getElementById("issue-to");
-const btnElement = document.getElementById('btn');
-const tableBody = document.querySelector("tbody");
-const books = [
-];
+const addnewrowbutton = document.getElementById("but")
 
-function handleEdit(event) {
-   const buttonElement  = event.target;
-   console.log(buttonElement);
-    const id = buttonElement.id;
-
-   if (buttonElement.textContent === 'Edit') {
-    event.target.textContent = "Save";
-    const parentElement = buttonElement.parentElement;
-
-    parentElement.removeChild(parentElement.firstChild);
-
-    const input = document.createElement("input");
-    input.id = 'status-inp';
-    input.value = books[id - 1].status;
-    parentElement.insertBefore(input, event.target);
-   } 
-   else{
-      const statusElement = document.getElementById('status-inp');
-      books[id - 1].status = statusElement.value;
-      renderBooksInsideTable();
-   }
-
-}
-function createTableRow (data, tableBody, bookId) {
-    
-
-    const tr = document.createElement("tr");
-    
-
-    const idTd = document.createElement("td");
-    idTd.textContent = bookId;
-
-    const bookNameTd = document.createElement("td");
-    bookNameTd.textContent = data.name;
-
-     const issueToTd = document.createElement("td");
-     issueToTd.textContent = data.issuedTo;
+const tablebody = document.getElementById("hello")
+const tableData = [];
 
 
-    const issuedAtTd = document.createElement("td");
-    issuedAtTd.textContent = data.issuedAt;
 
-    const statusTd = document.createElement("td");
-    statusTd.classList.add('flex');
+function submitdata(name, roll, subject, marks, markedby) {
+    addnewrowbutton.disabled = false
 
-    const button = document.createElement('button');
-    const span = document.createElement('span');
-
-    span.textContent = data.status;
-    const className = data.status === "not returned" ? "red" : "green";
-    span.classList.add(className);
-
-    button.textContent = 'Edit';
-    button.id = bookId;
-    button.addEventListener("click", handleEdit);
-
-    statusTd.appendChild(span);
-    statusTd.appendChild(button);
-     
-    // add these ths in tr 
-
-    tr.appendChild(idTd);
-    tr.appendChild(bookNameTd);
-    tr.appendChild(issueToTd);
-    tr.appendChild(issuedAtTd);
-    tr.appendChild(statusTd);
-
-    // add this tr in tbody
-    tableBody.appendChild(tr);
-}
-
-
-function renderBooksInsideTable() {
-
-   
-
-    tableBody.innerHTML = "";
-
-   books.map(function (book, index) {
-
-    
-    createTableRow(book, tableBody,index+1);
-   })
-}
-
-function handleFormSubmit () {
-    
-    const bookName = bookNameElement.value;
-    bookNameElement.value = "";
-
-    const issuedTo = issueToElement.value;
-    issueToElement.value = "";
-
-    
-
-    if(bookName && issuedTo) {
-        const book = {
-          name: bookName,
-          issuedTo: issuedTo,
-          issuedAt: new Date().toUTCString().substring(0,12),
-          status: "not returned",
-        };
-
-        books.push(book);
-        renderBooksInsideTable();
+    if (name == "" || roll == "" || marks == "" || markedby == "" || subject == "") {
+        alert("Please Fill all the fields");
+    }
+    else if (markedby.includes("@") == false) {
+        alert("Please Enter valid Email")
     }
     else {
-        alert("You are trying to enter empty details");
+        let data = { id: tableData.length + 1, student_name: name, student_roll: roll, subject: subject, marks: marks, markedBy: markedby }
+        console.log("New Row Entered:", data);
+        tableData.push(data);
+        console.log("All Table Data", tableData);
+        tablebody.innerHTML = tableData.map((item, ind) => {
+            return (` <tr> <td>${item.id}</td>
+            <td>${item.student_name}</td>
+            <td>${item.student_roll}</td>
+            <td>${item.subject}</td>
+            <td>${item.marks}</td>
+            <td>${item.markedBy}</td></tr>`)
+        }).join("")
+
     }
-    
+
 }
 
-btnElement.addEventListener('click', handleFormSubmit);
+
+addnewrowbutton.addEventListener("click", () => {
+
+    tablebody.innerHTML = tablebody.innerHTML + `
+  <td>${tableData.length + 1}</td>
+  <form >
+  <td><input id="stuname" type="text"/></td>
+  <td><input id="rollno" type="text"/></td>
+  <td><input id="subject" type="text"/></td>
+  <td><input id="marks" type="number"/></td>
+  <td><input id="markedby" type="email"/></td>
+  <td><button class="savebutt" onclick="submitdata(document.getElementById('stuname').value,document.getElementById('rollno').value,document.getElementById('subject').value,document.getElementById('marks').value,document.getElementById('markedby').value)">Save</button></td>
+  </form>`
+
+    addnewrowbutton.disabled = true
+})
